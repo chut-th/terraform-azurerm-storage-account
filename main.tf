48b4410a2531059b87ba136d882a2c8ab9389b64
chut-th <chutchawin.th@outlook.com>
2025-07-06 07:51:31 +0700

    Update required version and required providers

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "1.12.2"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.35.0"
+    }
+  }
+}
+
 locals {
   name = "${var.csp}${var.service}${var.company}${var.project}${var.environment}${var.running_number}"
 }
