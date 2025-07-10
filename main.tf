f6fb92e53ad2cef047bc0837b3ea0619d35d62cc
chut-th <chutchawin.th@outlook.com>
2025-07-10 10:39:36 +0700

    Add more arguments

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "1.12.2"
+  required_version = "~> 1.12.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.35.0"
+      version = "~> 4.35.0"
     }
   }
 }
@@ -13,9 +13,13 @@ locals {
 }
 
 resource "azurerm_storage_account" "sta" {
-  name                     = local.name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
+  name                             = local.name
+  resource_group_name              = var.resource_group_name
+  location                         = var.location
+  account_kind                     = var.account_kind # Optional, default: StorageV2
+  account_tier                     = var.account_tier
+  account_replication_type         = var.account_replication_type
+  cross_tenant_replication_enabled = var.cross_tenant_replication_enabled # Optional, default: false
+  shared_access_key_enabled        = var.shared_access_key_enabled        # Optional, default: true
+  public_network_access_enabled    = var.public_network_access_enabled    #Optional, default: true
 }
--- variables.tf
+++ variables.tf
@@ -40,6 +40,12 @@ variable "resource_group_name" {
   type        = string
 }
 
+variable "account_kind" {
+  description = "(Optional) Defines the Kind of account. Valid options are BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2. Defaults to StorageV2"
+  type        = string
+  default     = "StorageV2"
+}
+
 variable "account_tier" {
   description = "(Required) Defines the Tier to use for this storage account. Valid options are Standard and Premium. For BlockBlobStorage and FileStorage accounts only Premium is valid. Changing this forces a new resource to be created."
   type        = string
@@ -48,4 +54,22 @@ variable "account_tier" {
 variable "account_replication_type" {
   description = "(Required) Defines the type of replication to use for this storage account. Valid options are LRS, GRS, RAGRS, ZRS, GZRS and RAGZRS. Changing this forces a new resource to be created when types LRS, GRS and RAGRS are changed to ZRS, GZRS or RAGZRS and vice versa."
   type        = string
+}
+
+variable "cross_tenant_replication_enabled" {
+  description = "(Optional) Should cross Tenant replication be enabled? Defaults to false"
+  type        = string
+  default     = false
+}
+
+variable "shared_access_key_enabled" {
+  description = "(Optional) Indicates whether the storage account permits requests to be authorized with the account access key via Shared Key. If false, then all requests, including shared access signatures, must be authorized with Azure Active Directory (Azure AD). Defaults to true"
+  type        = string
+  default     = "true"
+}
+
+variable "public_network_access_enabled" {
+  description = "(Optional) Whether the public network access is enabled? Defaults to true"
+  type        = string
+  default     = "true"
 }
